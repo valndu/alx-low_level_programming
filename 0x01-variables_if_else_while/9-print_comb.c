09ff73b1009e49225df263e50865b856d9f63975
valndu <chibux1992@example.com>
2022-10-16 16:37:46 -0700

    my mandatory if else

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,10 @@
  */
 int main(void)
 {
-	int single_digit;
-	
+	int single_digit;	
 	for (single_digit = '0'; single_digit <= '9'; single_digit++)
 	{
 		putchar(single_digit);
-		
 		if (single_digit == '9')
 		{
 			break;
@@ -19,12 +17,9 @@ int main(void)
 		else
 		{
 			putchar(',');
-
 			putchar(' ');
 		}
 	}
-	
 	putchar('\n');
-	
 	return (0);
 }
