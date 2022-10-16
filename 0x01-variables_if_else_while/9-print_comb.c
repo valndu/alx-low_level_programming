7b250a50df264137364e332f71f16c30b3649ecf
valndu <chibux1992@example.com>
2022-10-16 16:09:41 -0700

    my mandatory if else

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main: Entry Point
+ * main- Entry Point
  *
  * Return: Always 0 (Success) 
  */
 int main(void)
 {
 	int single_digit;
-
+	
 	for (single_digit = '0'; single_digit <= '9'; single_digit++)
 	{
 		putchar(single_digit);
-
+		
 		if (single_digit == '9')
 		{
 			break;
