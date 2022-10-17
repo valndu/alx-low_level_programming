1227a17732ac75a21bdca2c63717e02f0bea3f41
valndu <chibux1992@example.com>
2022-10-17 04:07:32 -0700

    corrected update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,23 +1,20 @@
 #include<stdlib.h>
 #include<stdio.h>
-/*
- * main : Entry point
- *
- * Return 0: Always (Success)
- *
- * */
-int main (void)
+/**
+  *main - Entry point
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
 {
 	int  s_digit;
 
-		s_digit ='0';
-
-		while(s_digit <= '9' ) 
-		{
-			putchar(s_digit);
-			s_digit ++;
-		}
+	s_digit ='0';
+	while (s_digit <= '9')
+	{
+		putchar(s_digit);
+		s_digit++;
+	}
 		putchar('\n');
 		return (0);
-
 }
