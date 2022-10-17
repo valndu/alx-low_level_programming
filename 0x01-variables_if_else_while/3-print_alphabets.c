0d49930e714d0a6148820fb07e9bbded4dd9fa28
valndu <chibux1992@example.com>
2022-10-17 03:27:44 -0700

    correction update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,23 +5,22 @@
  *
  * Return: Always 0 (Success)
  */
- int main(void)
- {
+int main(void)
+{
 	char alph;
-    char ALPH;
-
-    alph = 'a';
-    ALPH = 'A';
-    while (alph <= 'z')
-    {
-	putchar(alph);
-	alph++;
-    }
-    while (ALPH <= 'Z')
-    {
-	putchar(ALPH);
+	char ALPH;
+	alph = 'a';
+	ALPH = 'A';
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	while (ALPH <= 'Z')
+	{
+		putchar(ALPH);
 		ALPH++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
