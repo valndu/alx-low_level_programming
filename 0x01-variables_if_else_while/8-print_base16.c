e065a9738a011b9906a6d665cf24b768ffc5093b
valndu <chibux1992@example.com>
2022-10-17 04:25:17 -0700

    corrected update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,25 +5,23 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int di_git;
 	char alph;
+
 	di_git = '0';
 	alph = 'a';
-
-	while(di_git <='9')
+	while (di_git <= '9')
 	{
 		putchar (di_git);
 		di_git++;
-	}	       
-       while(alph <= 'f')
-       {
-	       putchar (alph);
-	       alph ++;
-       }
-       putchar('\n');
-       return (0);
-
+	}
+	while (alph <= 'f')
+	{
+		putchar(alph);
+		alph++;
+	}
+		putchar('\n');
+		return (0);
 }
