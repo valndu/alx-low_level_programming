436cc02527626cfcce04e83a9c6567010b282a24
valndu <chibux1992@example.com>
2022-10-16 17:31:05 -0700

    error fix commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * main: Entry point
+ * main -  Entry point
  *
- * Return 0; Always (Success)
- *
- * */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	/*variable declaration*/
 	int di_git;
 	char alph;
-	/*initializing the char variables*/
-
 	di_git = '0';
 	alph = 'a';
-	       while(di_git <='9') 
-       {
-	       putchar (di_git);
-	       di_git ++;
-       }	       
+
+	while(di_git <='9')
+	{
+		putchar (di_git);
+		di_git++;
+	}	       
        while(alph <= 'f')
        {
 	       putchar (alph);
