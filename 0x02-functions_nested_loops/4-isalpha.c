fa07ddd9a23e6c2308473fcfa71d646f781338b0
valndu <chibux1992@example.com>
2022-10-19 09:48:16 -0700

    checking lowercaseoruppercase commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *_isalpha - to check alphabet character lowercase or uppercase
+  *
+  *@c: intiger that checks for lowercase or uppercase
+  *Return : 1 if c is a letter low/uppercase, 0 if not
+  */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
