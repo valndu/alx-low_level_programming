4aeb5f89efdb1d97de46dd5a5316d7136a3ded59
valndu <chibux1992@example.com>
2022-10-19 05:24:58 -0700

    my islower commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _islower - to check lowercase
+  *
+  *@c: int to print
+  *
+  *Return: 1 if lowercase,  0 if not
+  */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
