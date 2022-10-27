0c632f9689ff0345f28ef9ab30ccd9c8113e7e37
valndu <chibux1992@gmail.com>
2022-10-27 22:59:38 +0000

    my second commit on string concatinatio

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,8 +12,8 @@ char *string_toupper(char *s)
 
 	while (*(s + count) != '\0')
 	{
-		if (((s + count) >= 97) && ((s + count) <= 122))
-			(s + count) = *(s + count) - 32;
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
 		count++;
 	}
 	return (s);
